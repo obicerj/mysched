7b82c0d7e99faad4f1067740cb1cc08f3fecfe53
Jestoni Obice <obicerjestoni@gmail.com>
2024-11-04 15:00:24 -0500

    Add initial nav menu bar

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,11 @@
 import Image from "next/image";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import {
+    LightningBoltIcon,
+    CardStackPlusIcon,
+    StarIcon,
+} from "@radix-ui/react-icons";
 import { Calendar } from "@/components/ui/calendar";
 import { format, isToday } from "date-fns";
 import {
@@ -141,8 +146,7 @@ export default function Home() {
                         <AvatarImage src="https://avatars.githubusercontent.com/u/10084955?v=4" />
                         <AvatarFallback>JO</AvatarFallback>
                     </Avatar>
-                    <p className="pt-2">
-                        {/* {dayName}, {monthName} {today.getDate()} */}
+                    <p className="font-semibold text-sm pt-2">
                         {format(new Date(), "do MMMM Y")}
                     </p>
                 </div>
@@ -188,52 +192,39 @@ export default function Home() {
                 <div className="mt-4">{events}</div>
             </main>
 
-            <footer className="row-start-3 flex mt-12 gap-6 flex-wrap items-center justify-center">
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
+            <footer className="row-start-3 flex mt-12">
+                <div
+                    className="fixed bottom-4 z-10 mx-auto w-fit flex gap-6 flex-wrap items-center justify-center
+        border px-6 py-4 rounded-full border-amber-300 bg-amber-300"
                 >
-                    <Image
-                        aria-hidden
-                        src="/file.svg"
-                        alt="File icon"
-                        width={16}
-                        height={16}
-                    />
-                    Dashboard
-                </a>
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/window.svg"
-                        alt="Window icon"
-                        width={16}
-                        height={16}
-                    />
-                    Create Schedule
-                </a>
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/globe.svg"
-                        alt="Globe icon"
-                        width={16}
-                        height={16}
-                    />
-                    Categories
-                </a>
+                    <a
+                        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                        href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <LightningBoltIcon />
+                        Dashboard
+                    </a>
+                    <a
+                        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                        href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <CardStackPlusIcon />
+                        Create Schedule
+                    </a>
+                    <a
+                        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                        href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <StarIcon />
+                        Labels
+                    </a>
+                </div>
             </footer>
         </div>
     );
