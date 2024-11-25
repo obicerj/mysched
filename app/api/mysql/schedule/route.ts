ce95c5c43f6eda0431984668668fc2f2cf75ca1f
Jestoni Obice <obicerjestoni@gmail.com>
2024-11-24 21:09:24 -0500

    Add API initial setup for schedule

---
+++ app/api/mysql/schedule/route.ts
@@ -0,0 +1,44 @@
+import { NextResponse, NextRequest } from "next/server";
+
+import mysql from  'mysql2/promise';
+
+import { GetDBSettings, DBSettings } from "@/lib/utils";
+
+// connection parameters
+let connectionParams = GetDBSettings();
+
+export async function GET(request: Request) {
+    try {
+        // connect to db
+        const db = await mysql.createConnection(connectionParams);
+
+        // create query to fetch data
+        let get_exp_query = '';
+
+        get_exp_query = 'SELECT * FROM mysched.schedules';
+
+        // pass parameters to the sql query
+
+        let values: any[] = [];
+
+        const [results] = await db.execute(get_exp_query, values);
+
+        // close connection
+
+        db.end();
+
+        // return results as json api response
+        return NextResponse.json(results)
+
+    } catch (error) {
+        console.log('ERROR: API - ', (error as Error).message)
+        
+        const response = {
+            error: (error as Error).message,
+      
+            returnedStatus: 200,
+          }
+      
+          return NextResponse.json(response, { status: 200 })
+    }
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,47 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+export interface DBSettings {
+  host: string
+
+  port: number
+
+  user: string
+
+  password: string
+
+  database: string
+}
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const GetDBSettings = (): DBSettings => {
+  const env = process.env.NODE_ENV
+
+  if (env == 'development')
+    return {
+      host: process.env.DB_HOST!,
+
+      port: parseInt(process.env.DB_PORT!),
+
+      user: process.env.DB_USERNAME!,
+
+      password: process.env.DB_PASSWORD!,
+
+      database: process.env.DB_DATABASE!,
+    }
+  else
+    return {
+      host: process.env.DB_HOST!,
+
+      port: parseInt(process.env.DB_PORT!),
+
+      user: process.env.DB_USERNAME!,
+
+      password: process.env.DB_PASSWORD!,
+
+      database: process.env.DB_DATABASE!,
+    }
 }
