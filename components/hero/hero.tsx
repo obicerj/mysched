5fa9bc2f7802a51829ac30b897ff9ab143a11a89
Jestoni Obice <obicerjestoni@gmail.com>
2025-01-01 16:45:35 -0500

    Add initial design for landing page

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -17,7 +17,7 @@ export function Hero() {
                     rel="stylesheet"
                 />
             </Head>
-            <section className="flex items-center justify-center min-h-screen bg-white text-lime-600 text-center px-6">
+            <section className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 text-lime-600 text-center px-6">
                 <div>
                     <div className="w-full h-96 relative">
                         <Image
@@ -30,17 +30,18 @@ export function Hero() {
 
                     <h1 className="text-3xl sm:text-4xl font-extrabold leading-tight mb-6 text-gray-800">
                         Manage Your Schedule with{" "}
-                        <span className="sm:text-5xl font-extrabold font-caveat -rotate-3 inline-block text-gray-900 bg-lime-300 px-3 py-1 rounded-lg">
+                        <span className="sm:text-5xl font-extrabold font-caveat -rotate-3 inline-block text-amber-500 px-3 py-2 rounded-lg bg-white shadow-lg">
                             mySched
                         </span>
                     </h1>
-                    <p className="text-lg sm:text-xl mb-6 text-lime-600">
+                    <p className="text-lg sm:text-xl mb-6 text-gray-600">
                         Stay organized with a simple, intuitive scheduling tool.
                     </p>
 
                     <button
                         onClick={() => signIn()}
-                        className="bg-amber-500 text-white hover:bg-amber-600 px-6 py-3 rounded-full text-lg font-semibold transition duration-300"
+                        // className="bg-amber-500 text-white hover:bg-amber-600 px-6 py-3 rounded-full text-lg font-semibold transition duration-300"
+                        className="bg-amber-500 text-white px-6 py-3 rounded-full shadow-md hover:bg-amber-600 focus:ring-2 focus:ring-amber-300 focus:outline-none text-lg font-semibold"
                     >
                         Get Started
                     </button>
