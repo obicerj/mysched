fbe6b63114d25d5684bfcf65fbefe23bb93bccc0
Jestoni Obice <obicerjestoni@gmail.com>
2025-01-02 12:16:21 -0500

    Add drown down menu

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,7 +1,13 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { ChevronDownIcon } from "@radix-ui/react-icons";
 import { format } from "date-fns";
 import { signOut, useSession } from "next-auth/react";
-import Link from "next/link";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export function Header() {
     const clearClientSideCache = () => {
@@ -10,30 +16,63 @@ export function Header() {
     };
 
     const handleLogout = async () => {
-        // Sign out and optionally clear cache
-        await signOut({
-            callbackUrl: "/", // Redirect to the homepage after logout
-        });
-
-        // Perform any additional cache clearing actions here
+        await signOut({ callbackUrl: "/" }); // Redirect to the homepage after logout
         clearClientSideCache();
     };
 
     const { data: session } = useSession();
 
     const avatar = session ? session.user.image : "";
+    const name = session ? session.user.name : "Guest";
 
     return (
-        <div className="flex justify-between">
-            <div>
-                <Link href="/">
-                    <Avatar className="relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full">
-                        <AvatarImage src={avatar} />
-                        <AvatarFallback>JO</AvatarFallback>
-                    </Avatar>
-                </Link>
-                <button onClick={handleLogout}>Sign out</button>
-            </div>
+        <div className="flex justify-between items-center py-4 pl-2 pr-6">
+            <DropdownMenu>
+                <div className="relative">
+                    <DropdownMenuTrigger asChild className="cursor-pointer">
+                        <div className="absolute bottom-[-4px] left-1 transform -translate-x-1/2 z-20 h-4 w-4 rounded-full flex items-center justify-center shadow-lg border-2 border-white bg-gray-300">
+                            <ChevronDownIcon className="h-4 w-4 text-black" />
+                        </div>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuTrigger
+                        asChild
+                        className="relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full border-2 bg-gray-200 text-center items-center justify-center"
+                    >
+                        <div className="relative h-10 w-10">
+                            {/* Avatar Component */}
+                            <Avatar className="h-full w-full rounded-full cursor-pointer flex text-center items-center justify-center">
+                                <AvatarImage
+                                    src={avatar}
+                                    alt={`${name}'s avatar`}
+                                />
+                                <AvatarFallback>
+                                    {name.charAt(0).toUpperCase()}
+                                </AvatarFallback>
+                            </Avatar>
+                        </div>
+                    </DropdownMenuTrigger>
+                </div>
+                <DropdownMenuContent className="w-48">
+                    <DropdownMenuItem asChild>
+                        <a href="/profile" className="w-full text-left">
+                            Profile
+                        </a>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem asChild>
+                        <a href="/settings" className="w-full text-left">
+                            Settings
+                        </a>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem
+                        onClick={handleLogout}
+                        className="text-red-600"
+                    >
+                        Sign out
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+
+            {/* Date Section */}
             <p className="font-semibold text-sm text-right">
                 {format(new Date(), "do MMMM")} <br />
                 {format(new Date(), "y")}
